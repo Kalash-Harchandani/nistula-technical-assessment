0efb92b9630edec777ac93dfeef954610185884f
Kalash-Harchandani <kalashharchandani@gmail.com>
2026-05-14 11:18:50 +0530

    Add unified message normalization system

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,18 +1,19 @@
+import normalizeMessage from "../utils/normalizeMessage.js";
 export const handleIncomingMessage = async (req, res) => {
   try {
 
     const payload = req.body;
+    const normalizedMessage = normalizeMessage(payload);
 
-    console.log(payload);
+    console.log("Normalized Message:", normalizedMessage);
 
     return res.status(200).json({
       success: true,
-      message: "Webhook received successfully",
-      data: payload
+      data: normalizedMessage
     });
 
   } catch (error) {
-
+    console.log("Controller Error:", error);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error"
