6e06198b663a1e44f17e991037cca23a9d35ad97
Kalash-Harchandani <kalashharchandani@gmail.com>
2026-05-14 12:10:55 +0530

    Clean repository structure and finalize assessment submission

--- routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleIncomingMessage } from "../controllers/messageController.js";
-
-const router = express.Router();
-
-router.post("/message", handleIncomingMessage);
-
-export default router;
